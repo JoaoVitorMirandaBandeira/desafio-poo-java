f3136a13d1225c79d05deeca20feb1f443a05cc4
JoaoVitorMirandaBandeira <joaovitormirandabandeira@gmail.com>
2026-05-11 20:13:47 -0300

    feat(dominio): adiciona classes Bootcamp, Conteudo e Dev
    - cria classe Bootcamp com atributos e métodos
    - define classe abstrata Conteudo com XP_PADRAO
    - implementa equals e hashCode em todas as novas classes

---
+++ src/br/com/dio/desafio/dominio/Dev.java
@@ -0,0 +1,48 @@
+package br.com.dio.desafio.dominio;
+
+import javax.swing.plaf.synth.ColorType;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class Dev {
+    private String nome;
+    private Set<Conteudo> conteudosInscrito = new LinkedHashSet<>();
+    private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Set<Conteudo> getConteudosInscrito() {
+        return conteudosInscrito;
+    }
+
+    public void setConteudosInscrito(Set<Conteudo> conteudosInscrito) {
+        this.conteudosInscrito = conteudosInscrito;
+    }
+
+    public Set<Conteudo> getConteudosConcluidos() {
+        return conteudosConcluidos;
+    }
+
+    public void setConteudosConcluidos(Set<Conteudo> conteudosConcluidos) {
+        this.conteudosConcluidos = conteudosConcluidos;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Dev dev = (Dev) o;
+        return Objects.equals(nome, dev.nome) && Objects.equals(conteudosInscrito, dev.conteudosInscrito) && Objects.equals(conteudosConcluidos, dev.conteudosConcluidos);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, conteudosInscrito, conteudosConcluidos);
+    }
+}
