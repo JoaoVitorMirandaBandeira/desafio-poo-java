bd289eb782e99580b171b773a2f2aacd7bfe827e
JoaoVitorMirandaBandeira <joaovitormirandabandeira@gmail.com>
2026-05-11 15:15:55 -0300

    Added `Curso` and `Mentoria` classes with necessary attributes and methods. Updated `Main.java` to demonstrate the usage of these new classes. Adjusted project SDK version in `.idea/misc.xml`.

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,21 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
-    }
+        Curso curso = new Curso("Java", "Curso de java", 60);
+        System.out.println(curso);
 
+        LocalDate dataMentoria = LocalDate.now().plusDays(2);
+        System.out.println(LocalDate.now());
+        System.out.println(dataMentoria);
+        Mentoria mentoria = new Mentoria("Java na pratica", "Demonstrando uso de java", dataMentoria);
+        System.out.println(mentoria);
+
+
+    }
 }
